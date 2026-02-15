accea3860283b667e3e08fa65ebc33c41ed34c1d
subtotalpo2662 <240177963+subtotalpo2662@users.noreply.github.com>
2026-02-15 09:34:36 +0000

    Added DB and controls

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import { drizzle } from "drizzle-orm/d1";
+import { D1Database } from "@cloudflare/workers-types";
+
+import * as schema from "./schema";
+
+declare global{
+    interface CloudflareEnv{
+        DB: D1Database;       
+    }
+}
+
+export function getDb( env:{DB:D1Database}){
+    return drizzle(env.DB,{schema});
+}
+
+export {schema};
